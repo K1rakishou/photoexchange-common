8ad42f32ffb7182904568bc2621d0277b06120f5
k1rakishou <kez1911@protonmail.com>
2018-12-02 17:17:25 +0300

    Remove unused error codes Add YouAreBanned Add error code messages

--- src/main/java/core/ErrorCode.kt
+++ src/main/java/core/ErrorCode.kt
@@ -1,20 +1,29 @@
 package core
 
+import java.lang.IllegalStateException
+
 enum class ErrorCode(val value: Int) {
   UnknownError(0),
   Ok(1),
   BadRequest(2),
   DatabaseError(3),
-  CameraIsNotAvailable(4),
-  CameraIsNotStartedException(5),
-  TimeoutException(6),
-  CouldNotTakePhoto(7),
-  NoPhotosInRequest(8),
-  NoPhotosToSendBack(9),
-  UserDoesNotHaveFirebaseToken(10);
+  NoPhotosInRequest(4),
+  NoPhotosToSendBack(5),
+  UserDoesNotHaveFirebaseToken(6),
+  YouAreBanned(7);
 
   fun getErrorMessage(): String {
-    TODO()
+    return when(this) {
+      UnknownError -> "Unknown error."
+      BadRequest -> "Bad request. Check your input parameters."
+      DatabaseError -> "Server database error."
+      NoPhotosInRequest -> "No photos were provided in the request."
+      NoPhotosToSendBack -> "No photos to send back"
+      UserDoesNotHaveFirebaseToken -> "User have not registered yet."
+      YouAreBanned -> "You are banned."
+
+      Ok -> throw IllegalStateException("This should not happen")
+    }
   }
 
   companion object {
