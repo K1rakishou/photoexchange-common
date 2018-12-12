fb786d004faa1ce63396034193804f4833b56d4b
k1rakishou <kez1911@protonmail.com>
2018-12-12 18:17:41 +0300

    Remove some redundant shared constants

--- src/main/java/core/SharedConstants.kt
+++ src/main/java/core/SharedConstants.kt
@@ -3,16 +3,8 @@ package core
 import java.util.concurrent.TimeUnit
 
 object SharedConstants {
-
   const val MAX_PHOTO_SIZE = 10 * (1024 * 1024) //10 megabytes
-  
+
   //TODO: change in production
   val OLD_PHOTOS_CLEANUP_ROUTINE_INTERVAL = TimeUnit.MINUTES.toMillis(1)
-  //TODO: change in production
-  val UPLOADED_OLDER_THAN_TIME_DELTA = TimeUnit.MINUTES.toMillis(1)
-  //TODO: change in production
-  val DELETED_EARLIER_THAN_TIME_DELTA = TimeUnit.MINUTES.toMillis(1)
-
-  const val PHOTOS_DELIMITER = ','
-
 }
