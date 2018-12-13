e7225f7f5ea2d18f7461c7d3d8626a3c021beeab
k1rakishou <kez1911@protonmail.com>
2018-12-13 16:37:36 +0300

    GalleryPhotoInfoResponse -> PhotoAdditionalInfoResponse

--- src/main/java/net/response/PhotoAdditionalInfoResponse.kt
+++ src/main/java/net/response/PhotoAdditionalInfoResponse.kt
@@ -4,28 +4,27 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import core.ErrorCode
 
-class GalleryPhotoInfoResponse
+class PhotoAdditionalInfoResponse
 private constructor(
 
   @Expose
-  @SerializedName("gallery_photos_info")
-  val galleryPhotosInfo: List<GalleryPhotosInfoResponseData>,
+  @SerializedName("photo_additional_info")
+  val photoAdditionalInfo: List<PhotoAdditionalInfoResponseData>,
 
   errorCode: ErrorCode
 ) : StatusResponse(errorCode.value) {
 
   companion object {
-    fun success(galleryPhotosInfo: List<GalleryPhotosInfoResponseData>): GalleryPhotoInfoResponse {
-      return GalleryPhotoInfoResponse(galleryPhotosInfo, ErrorCode.Ok)
+    fun success(photoAdditionalInfo: List<PhotoAdditionalInfoResponseData>): PhotoAdditionalInfoResponse {
+      return PhotoAdditionalInfoResponse(photoAdditionalInfo, ErrorCode.Ok)
     }
 
-    fun fail(errorCode: ErrorCode): GalleryPhotoInfoResponse {
-      return GalleryPhotoInfoResponse(emptyList(), errorCode)
+    fun fail(errorCode: ErrorCode): PhotoAdditionalInfoResponse {
+      return PhotoAdditionalInfoResponse(emptyList(), errorCode)
     }
   }
 
-  class GalleryPhotosInfoResponseData(
-
+  class PhotoAdditionalInfoResponseData(
     @Expose
     @SerializedName("photo_name")
     val photoName: String,
