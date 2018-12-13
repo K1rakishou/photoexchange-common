04f678a497649585a177a6ecdd9bcdf3c7ade694
k1rakishou <kez1911@protonmail.com>
2018-12-13 15:46:02 +0300

    Remove OLD_PHOTOS_CLEANUP_ROUTINE_INTERVAL

--- src/main/java/core/SharedConstants.kt
+++ src/main/java/core/SharedConstants.kt
@@ -1,10 +1,5 @@
 package core
 
-import java.util.concurrent.TimeUnit
-
 object SharedConstants {
   const val MAX_PHOTO_SIZE = 10 * (1024 * 1024) //10 megabytes
-
-  //TODO: change in production
-  val OLD_PHOTOS_CLEANUP_ROUTINE_INTERVAL = TimeUnit.MINUTES.toMillis(1)
 }
