18be4d1a5212e05a1eb8079e4430aee02daa2342
k1rakishou <kez1911@protonmail.com>
2018-12-18 18:24:30 +0300

    Introduce MAX_PACKET_SIZE Make MAX_PHOTO_SIZE and MAX_PACKET_SIZE Long values

--- src/main/java/core/SharedConstants.kt
+++ src/main/java/core/SharedConstants.kt
@@ -1,6 +1,8 @@
 package core
 
 object SharedConstants {
-  const val MAX_PHOTO_SIZE = 10 * (1024 * 1024) //10 megabytes
+  const val MAX_PHOTO_SIZE = (10 * (1024 * 1024)).toLong() //10 megabytes
+  const val MAX_PACKET_SIZE = (1024 * 256).toLong() //256 kb
+
   const val NO_GOOGLE_PLAY_SERVICES_DEFAULT_TOKEN = "NO_GOOGLE_PLAY_SERVICES_DEFAULT_TOKEN"
 }
