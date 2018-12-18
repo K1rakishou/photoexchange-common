17cf32f44bb680a46e85f9c6968746a57e54da8e
k1rakishou <kez1911@protonmail.com>
2018-12-18 17:43:11 +0300

    Move NO_GOOGLE_PLAY_SERVICES_DEFAULT_TOKEN to shared constants

--- src/main/java/core/SharedConstants.kt
+++ src/main/java/core/SharedConstants.kt
@@ -2,4 +2,5 @@ package core
 
 object SharedConstants {
   const val MAX_PHOTO_SIZE = 10 * (1024 * 1024) //10 megabytes
+  const val NO_GOOGLE_PLAY_SERVICES_DEFAULT_TOKEN = "NO_GOOGLE_PLAY_SERVICES_DEFAULT_TOKEN"
 }
