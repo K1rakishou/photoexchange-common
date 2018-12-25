13b9879acf116665f99ee6d88db8dba454c903b0
k1rakishou <kez1911@protonmail.com>
2018-12-25 18:33:17 +0300

    Add USER_ID_LEN and PHOTO_NAME_LEN to SharedConstants

--- src/main/java/core/SharedConstants.kt
+++ src/main/java/core/SharedConstants.kt
@@ -5,4 +5,7 @@ object SharedConstants {
   const val MAX_PACKET_SIZE = (1024 * 256).toLong() //256 kb
 
   const val NO_GOOGLE_PLAY_SERVICES_DEFAULT_TOKEN = "NO_GOOGLE_PLAY_SERVICES_DEFAULT_TOKEN"
+
+  const val USER_ID_LEN = 20
+  const val PHOTO_NAME_LEN = 32
 }
