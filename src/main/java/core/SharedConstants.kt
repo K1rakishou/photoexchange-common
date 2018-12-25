75d2d6b4642b5bdb3a16c7b70052d88280110c99
k1rakishou <kez1911@protonmail.com>
2018-12-25 18:52:47 +0300

    Decrease max packet size down to 64kb

--- src/main/java/core/SharedConstants.kt
+++ src/main/java/core/SharedConstants.kt
@@ -2,7 +2,7 @@ package core
 
 object SharedConstants {
   const val MAX_PHOTO_SIZE = (10 * (1024 * 1024)).toLong() //10 megabytes
-  const val MAX_PACKET_SIZE = (1024 * 256).toLong() //256 kb
+  const val MAX_PACKET_SIZE = (1024 * 64).toLong() //64 kb
 
   const val NO_GOOGLE_PLAY_SERVICES_DEFAULT_TOKEN = "NO_GOOGLE_PLAY_SERVICES_DEFAULT_TOKEN"
 
